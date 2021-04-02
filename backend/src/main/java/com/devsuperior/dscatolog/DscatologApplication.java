0d4257bfc2fce5d00b8eb50802ea28734813a75a
filipe santos <fhilips.souza@gmail.com>
2021-04-02 16:54:36 -0300

    firt upload test

--- backend/src/main/java/com/devsuperior/dscatolog/DscatologApplication.java
+++ backend/src/main/java/com/devsuperior/dscatolog/DscatologApplication.java
@@ -1,13 +1,25 @@
 package com.devsuperior.dscatolog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DscatologApplication {
+import com.devsuperior.dscatolog.services.S3Service;
 
+@SpringBootApplication
+public class DscatologApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 	public static void main(String[] args) {
 		SpringApplication.run(DscatologApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		s3Service.uploadFile("c:\\Users\\filip\\Pictures\\Filipe\\foto.jpg");
 	}
 
 }
