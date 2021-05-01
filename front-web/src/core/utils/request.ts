aa9e21045979c79a8b7ec1859845c08a490623cc
filipe santos <fhilips.souza@gmail.com>
2021-05-01 17:44:55 -0300

    Make login and save session data

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,20 +1,47 @@
 import axios, { Method } from "axios";
+import { CLIENT_ID, CLIENT_SECRET } from "./auth";
+import qs from 'qs'
 
 type RequesParams = {
     method?: Method;
     url: string;
-    data?: object;
+    data?: object | string;
     params?: object;
+    headers?: object;
 }
 
-const BASE_URL = "http://localhost:3000";
+type LoginData = { 
+    username: string;
+    password: string;
+}
+
+const BASE_URL = "http://localhost:8080";
 
 
-export const makeRequest = ({ method = 'GET', url, data, params }: RequesParams) => {
+export const makeRequest = ({ method = 'GET', url, data, params, headers }: RequesParams) => {
     return axios({
         method,
         url: `${BASE_URL}${url}`,
         data,
-        params
+        params, 
+        headers
     })
 }
+
+export const makeLogin = (loginData: LoginData) => {
+  const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
+
+  const headers = {
+    Authorization: `Basic ${window.btoa(token)}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  const payload = qs.stringify({ ...loginData, grant_type: 'password' });
+
+  return makeRequest({
+    url: '/oauth/token',
+    data: payload,
+    method: 'POST',
+    headers,
+  });
+};
