39fbf6e7217fecc42bb1cd7acf5349b18c38e32f
fhilips <fhilips.souza@gmail.com>
2021-06-06 23:22:03 -0300

    Fix Product Filters

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -5,19 +5,22 @@ import { makeRequest } from "core/utils/request";
 import { useEffect, useState } from "react";
 import { Category } from "core/types/Product";
 
-export type FilterForm = {
-  name?: string;
-  categoryId?: number;
-}
-
 type Props = {
-  onSearch: (filter: FilterForm) => void;
+  name?: string;
+  category?: Category;
+  handleChangeName: (name: string) => void;
+  handleChangeCategory: (category: Category) => void;
+  clearFilters: () => void;
 }
-const ProductFilters = ({ onSearch }: Props) => {
+const ProductFilters = ({
+   name, 
+   handleChangeName, 
+   category, 
+   handleChangeCategory,
+   clearFilters
+   }: Props) => {
   const [isLoadingCategories, setIsLoadingCategories] = useState(false);
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState<Category>();  
+  const [categories, setCategories] = useState<Category[]>([]);  
 
   useEffect(() => {
     setIsLoadingCategories(true);
@@ -30,19 +33,6 @@ const ProductFilters = ({ onSearch }: Props) => {
       });
   }, []);
 
-  const handleChangeName = (name: string) => {
-    setName(name)
-    onSearch({ name, categoryId: category?.id })
-  }
-  const handleChangeCategory = (category: Category) => {
-    setCategory(category)
-    onSearch({ name, categoryId: category?.id })
-  }
-  const clearFilters = () => {
-    setCategory(undefined);
-    setName('');
-    onSearch({ name: '', categoryId: undefined });
-  }
   return (
     <div className="card-base product-filters-container">
       <div className="input-search">
--- front-web/src/core/pages/Catalog/index.tsx
+++ front-web/src/core/pages/Catalog/index.tsx
@@ -5,9 +5,9 @@ import ProductCard from "./components/ProductCard";
 import Pagination from "core/components/Pagination";
 import "./styles.scss";
 import { makeRequest } from "core/utils/request";
-import { ProductsResponse } from "core/types/Product";
+import { Category, ProductsResponse } from "core/types/Product";
 import ProductCardLoader from "./components/Loaders/ProductCardLoader";
-import ProductFilters, { FilterForm } from "core/components/ProductFilters";
+import ProductFilters from "core/components/ProductFilters";
 
 
 
@@ -15,13 +15,15 @@ const Catalog = () => {
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState<Category>();  
   
-  const getProducts = useCallback((filter?: FilterForm) => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 12,
-      name: filter?.name,
-      categoryId: filter?.categoryId
+      name,
+      categoryId: category?.id
     };
     setIsLoading(true);
     makeRequest({ url: "/products", params })
@@ -29,11 +31,26 @@ const Catalog = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, [activePage])
+  }, [activePage, name, category])
 
   useEffect(() => {
     getProducts();
   }, [getProducts]);
+
+  
+  const handleChangeName = (name: string) => {
+    setActivePage(0)
+    setName(name)    
+  }
+  const handleChangeCategory = (category: Category) => {
+    setActivePage(0)
+    setCategory(category)    
+  }
+  const clearFilters = () => {
+    setActivePage(0)
+    setCategory(undefined);
+    setName('');   
+  }
 
   return (
     <div className="catalog-container">
@@ -41,7 +58,13 @@ const Catalog = () => {
         <h1 className="catalog-title">
           Catálogo de produtos
         </h1>
-        <ProductFilters onSearch={filter => getProducts(filter)} />
+        <ProductFilters 
+          name={name}
+          category={category}
+          handleChangeName={handleChangeName}
+          handleChangeCategory={handleChangeCategory}
+          clearFilters={clearFilters}
+          />
       </div>
       <div className="catalog-products">
         {isLoading ? (
