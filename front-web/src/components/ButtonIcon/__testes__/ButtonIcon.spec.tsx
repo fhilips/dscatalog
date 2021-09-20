1636e27770613fc7d65587654ed8b4145fd803ef
Filipe dos Santos <fhilips.souza@gmail.com>
2021-09-20 15:48:51 -0300

    ButtonIcon test

---
+++ front-web/src/components/ButtonIcon/__testes__/ButtonIcon.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import ButtonIcon from '..'
+
+test('ButtonIcon should render button with given text', () => {
+
+  const text = "Fazer Login";
+  render(     
+    <ButtonIcon text={text} />
+  );
+  
+  expect(screen.getByText(text)).toBeInTheDocument();
+})
