75dc22b7cbbedf0e583cf87086e1ca9948fa1559
fhilips <fhilips.souza@gmail.com>
2021-09-20 23:00:17 -0300

    tests completed

---
+++ front-web/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import Catalog from "..";
+import history from 'util/history';
+import { Router } from 'react-router-dom';
+import { server } from './fixtures';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('should render Catalog with products', async () => {
+
+    render(
+        <Router history={history}>
+             <Catalog />
+        </Router>
+    );
+
+    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
+
+    await waitFor(() => {
+        expect(screen.getByText('Smart TV')).toBeInTheDocument();
+    });
+});
